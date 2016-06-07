8d62f9bc9ed4bd0503226408c64626994f5e3d54
Peter C <peter.chau.esq@gmail.com>
2016-06-07 06:46:54 +0000

    better ux

--- imports/components/GameInfo.jsx
+++ imports/components/GameInfo.jsx
@@ -6,7 +6,7 @@ const GameInfo = React.createClass({
       <div className="game-info">
         <p className="subtitle is-4">Generations: {this.props.generations}</p>
         <p className="subtitle is-4">{this.props.controls.boardSize.rows} x {this.props.controls.boardSize.columns}</p>
-        <p className="subtitle is-4">Game Speed: {this.props.controls.gameSpeed}ms</p>
+        <p className="subtitle is-4">Game Speed: {+this.props.controls.gameSpeed.toFixed(2)}ms</p>
       </div>
     )
   }
