e710398bbba48e2063201dd7736df5656ec21220
Peter C <peter.chau.esq@gmail.com>
2016-06-07 06:03:41 +0000

    make user not scalable

--- imports/components/Main.jsx
+++ imports/components/Main.jsx
@@ -4,23 +4,23 @@ const Main = React.createClass({
   render () {
     return (
       <section className="hero is-primary is-bold is-fullheight">
-          <div className="container">
-            <h1 className="title is-1">
-              Conway's Game of Life
-            </h1>
-              {React.cloneElement(this.props.children, this.props)}
-          </div>
-          <div className="hero-foot">
-            <nav className="tabs is-boxed is-fullwidth">
-              <div className="container">
-                <a href="https://github.com/PeterChauYEG/gameoflife" className="button is-primary is-outlined">
-                  <span class="icon">
-                    <i className="fa fa-github"> Conway's Game of Life</i>
-                  </span>
-                </a>
-              </div>
-            </nav>
-          </div>          
+        <div className="container">
+          <h1 className="title is-1">
+            Conway's Game of Life
+          </h1>
+            {React.cloneElement(this.props.children, this.props)}
+        </div>
+        <div className="hero-foot">
+          <nav className="tabs is-boxed is-fullwidth">
+            <div className="container">
+              <a href="https://github.com/PeterChauYEG/gameoflife" className="button is-primary is-outlined">
+                <span class="icon">
+                  <i className="fa fa-github"> Conway's Game of Life</i>
+                </span>
+              </a>
+            </div>
+          </nav>
+        </div>          
       </section>      
     )
   }
